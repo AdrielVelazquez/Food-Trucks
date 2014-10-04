f9a9de3e0d3a8edd5ad6c4936b078b336ef304e4
adriel <adriel@adamplusco.com>
2014-10-03 23:03:23 -0400

    Found JSON documentation of the schedule, which is going through a transformation to extract exact times.

--- ingestion.py
+++ ingestion.py
@@ -1,36 +1,57 @@
-from StringIO import StringIO
+import csv
+import logging
 import urllib2
-from urllib2 import Request
 
-from bs4 import BeautifulSoup
+import requests
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def convert_hours(schedule_key, schedule_value):
+    if schedule_key == "Hours":
+        print schedule_value
+        return schedule_key, schedule_value
+    return schedule_key, schedule_value
 
 # Parse the response
-
-def extract_pdf_schedule(permit_number):
+def get_schedule(permit_number, location_id):
     '''
-    The contents of the scedule are stored in a PDF file...unfortunately.
-    I'm going to read and parse the PDF on start, and create the database from
-    the extracted data. This step may take a long time; however,
-    it will be updated via crons/jenkins if fully deployed
+    Convert the extracted information into proper time stamps
+    Military Time is going to be used.
+    The times are going to changed to the following,
+    morning = 6am - 10am
+    Lunch = 10am -
     '''
+    json_url = "http://bsmnt.sfdpw.org/ArcGIS/rest/services/MobileFoodPermits/MapServer/0/query?f=json&where=DataField%20%3D%20%27{}%27&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=DataField%2CLocationID%2CInfoField".format(permit_number)
+    r = requests.get(json_url)
+    locations = r.json()
 
-    custom_url = 'http://bsmnt.sfdpw.org/reportviewer/?title=form&report=/BSM/BSMReports/' \
-                 'MFFPermitLocationInfo&ExportPDF=1&filename=form&params=permit={}'.format(permit_number)
+    for item in locations['features']:
+        if item['attributes']['LocationID'] == location_id:
+            schedule_dict = {}
+            for key_value in item['attributes']['InfoField'].split(","):
+                if "FoodItems" not in key_value:
+                    try:
+                        temp_key, temp_value = key_value.split(": ")
+                        temp_key, temp_value = convert_hours(temp_key, temp_value)
+                        schedule_dict[temp_key] = temp_value
+                    except ValueError:
+                        logger.warning("Value doesn't contain a semicolon")
 
-    open = urllib2.urlopen(Request(custom_url)).read()
-    memoryFile = StringIO(open).read()
-    request = urllib2.Request('http://pdfx.cs.man.ac.uk', memoryFile, headers={'Content-Type': 'application/pdf'})
-    response = urllib2.urlopen(request).read()
-    schedule_soup = BeautifulSoup(response)
-    for item in schedule_soup.find_all('outsider'):
-        if "AM" in item.string or "PM" in item.string:
-            print item.string.split(":")
+                else:
+                    schedule_dict["FoodItems"] = key_value.replace("FoodItems: ", "")
+            return schedule_dict
 
 def ingest_csv_file():
     '''
     Each line links to a PDF with a specific schedule on the food truck.
     It appears that the location modifies according to the date.
     '''
+    csv_url = "https://data.sfgov.org/api/views/px6q-wjh5/rows.csv?accessType=DOWNLOAD"
+    response = urllib2.urlopen(csv_url)
+    master_doc = csv.DictReader(response)
+    for row in master_doc:
+        schedule = get_schedule(row['permit'], int(row['locationid']))
 
-extract_pdf_schedule('14MFF-0034')
+logging.info("Creating local database")
+ingest_csv_file()
